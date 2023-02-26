d75f7fcf66f6d5b2db57ccb0216006df6feb5d6b
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-02-26 10:08:00 +0100

    2023_02_26_GIT_Objects - commit 02

--- 2023_02_27_rakieta.py
+++ 2023_02_27_rakieta.py
@@ -1,4 +1,28 @@
-class Rocket:
-    pass
+from random import randint
 
-rakieta1 = Rocket()
+class Rocket:
+    def __init__(self):
+        self.altitude = 0
+
+    def __str__(self):
+        return "Hejka, jestem rakietą! I znajduje się na wysokości " + str(self.altitude)
+
+    def move(self):
+        self.altitude += 1
+
+
+rakiety = []
+
+for i in range(5):
+    nowaRakieta = Rocket()
+    rakiety.append(nowaRakieta)
+
+for rakieta in rakiety:
+    print(rakieta)
+
+for i in range(100):
+    indexDoPoruszania = randint(0, 4)
+    rakiety[indexDoPoruszania].move()
+
+for rakieta in rakiety:
+    print("Moja aktualna wysokość to: ", rakieta.altitude)
