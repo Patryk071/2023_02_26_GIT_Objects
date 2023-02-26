38d7365eae02ecb6b504670f74d282f2b7784bec
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-02-26 11:13:52 +0100

    2023_02_26_GIT_Objects - commit 04, import z pliku

--- 2023_02_27_Imp.py
+++ 2023_02_27_Imp.py
@@ -1,6 +1,11 @@
 #Import z pliku
 
 with open("imionanazwiska.txt", "r", encoding="UTF-8") as file:
-    cala_tresc = file.read()
+    #cala_tresc = file.read()
+    for line in file:
+        print(line.replace("\n", "").split())
 
-print(cala_tresc)
+#print(cala_tresc)
+
+with open("imiona.txt", "w", encoding="UTF-8") as file:
+    pass
