536809c0ef22f304615e275b097ba066eb3ee92a
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-03-07 17:12:11 +0100

    Commit no. 007 - 07.03.2023

--- 2023_02_26_Imp.py
+++ 2023_02_26_Imp.py
@@ -1,11 +1,33 @@
 #Import z pliku
 
+listaOsob = []
+
 with open("imionanazwiska.txt", "r", encoding="UTF-8") as file:
     #cala_tresc = file.read()
     for line in file:
-        print(line.replace("\n", "").split())
+        listaOsob.append(tuple(line.replace("\n", "").split()))
 
-#print(cala_tresc)
+print(listaOsob)
+print(listaOsob[0])     ##osoba na miejscu 0
+print(listaOsob[0][0])  #imię osoby na miejscu 0
 
+# Zapisanie imion z list osób do pliku "imiona.txt"
 with open("imiona.txt", "w", encoding="UTF-8") as file:
-    pass
+    for item in listaOsob:
+        file.write(item[0] + "\n")
+
+# Zapisanie nazwisk z list osób do pliku "nazwiska.txt"
+with open("nazwiska.txt", "w", encoding="UTF-8") as file:
+    for item in listaOsob:
+        # ZAPIS warunku z IF
+        #if len(item) == 2: ##ten warunek jest niezbędny w przypadku,
+        #                    ## gdy w tupli brak nazwiska
+        #    file.write(item[1] + "\n")
+        #else:
+        #    file.write("\n")
+
+        # ZAPIS warunku z try (obsługa wyjątków)
+        try:
+            file.write(item[1] + "\n")
+        except IndexError:
+            file.write("\n")
