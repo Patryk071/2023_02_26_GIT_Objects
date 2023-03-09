b8c7c05a6744ffdf9a402c1a3592599820d872c2
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-03-09 23:37:58 +0100

    Commit no. 011 - 09.03.2023, sterowanie przeglądarką z kodu

--- 2023_02_26_testy.py
+++ 2023_02_26_testy.py
@@ -2,8 +2,19 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome()
+
 driver.get("http://saucedemo.com")
 print(driver.title)
+
+# odwoływanie się do elementów po xPath
+user = driver.find_element('xpath', '//*[@id="user-name"]')
+user.send_keys("standard_user")
+
+login = driver.find_element('xpath', '//*[@id="password"]')
+login.send_keys("secret_sauce")
+
+logmein = driver.find_element('xpath', '//*[@id="login-button"]')
+logmein.click()
 
 time.sleep(5)
 driver.quit()
