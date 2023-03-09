7af8f39395dad645bdaae48dc85c82610b0d0baa
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-03-09 17:44:57 +0100

    Commit no. 009 - 09.03.2023, sterowanie przeglądarką z kodu

--- 2023_02_26_temp.py
+++ 2023_02_26_temp.py
@@ -1,3 +1,24 @@
+import time
 from selenium import webdriver
 
 driver = webdriver.Chrome()
+
+# Otwiera przeglądarkę i przenosi na wskazaną stronę www
+driver.get("https://www.google.pl")
+print("Jesteś w środku kodu")
+
+# Musimy znaleźć id przycisku "akceptuj" i skąd bierzemy nazwę
+buttonAccept = driver.find_element("id", "L2AGLb")
+buttonAccept.click()
+
+searchInput = driver.find_element("name", "q")
+searchInput.send_keys("Aktualna pogoda w Krakowie")
+
+buttonSearch = driver.find_element("name", "btnK")
+buttonSearch.submit()
+
+time.sleep(5)
+print("Jesteś na końcu kodu")
+
+# Zamyka przeglądarkę
+driver.quit()
